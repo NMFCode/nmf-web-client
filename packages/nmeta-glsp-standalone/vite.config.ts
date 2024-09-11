ccb58455c2f00eb053419afe11a5a5a300d5281b
Georg Hinkel <georg.hinkel@hs-rm.de>
2024-09-11 09:31:13 +0200

    force dependency changes

--- packages/nmeta-glsp-standalone/vite.config.ts
+++ packages/nmeta-glsp-standalone/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
-        commonjsOptions: { include: [/nmeta-glsp-client/, /\@eclipse-glsp\/client/] },
+        commonjsOptions: { include: [/nmeta-glsp-client/, /@eclipse-glsp\/client/] },
     },
     optimizeDeps: {
         include: ['nmeta-glsp-client', '@eclipse-glsp/client'],
